bef0c2eefda9007fed07568c81e3aa8d22d9777e
Joshua Kim <jykim92@gmail.com>
2020-04-09 21:57:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ row_index = 0
 
 while row_index < src.count do
   element_index = 0
-  while element_index < src[row_index] do
+  while element_index < src[row_index].count do
     if src[row_index][element_index].class == String
     new_array << src[row_index][element_index]
     element_index += 1
