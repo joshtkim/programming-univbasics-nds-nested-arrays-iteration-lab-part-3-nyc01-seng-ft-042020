f77605215b23989995ca374d8a4e61b6ff8da41d
Joshua Kim <jykim92@gmail.com>
2020-04-09 21:38:23 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,13 @@
 def join_nested_strings(src)
-src.join(" ")
-src
+new_array = []
+
+row_index = 0
+
+while row_index < src.count do
+  element_index = 0
+  while element_index < src[row_index].count do
+    src.join(" ")
+    element_index += 1
+  end
+  row_index += 1
 end
